fa977a299f246ce36e71a7d96daada8d8d8dda86
knge2 <knge2@DESKTOP-74VA7T2>
2021-07-18 02:13:02 +0900

    실험2

--- doshop/src/main/java/spring/intercepteor/InterceptorFactory.java
+++ doshop/src/main/java/spring/intercepteor/InterceptorFactory.java
@@ -10,7 +10,7 @@ import spring.intercepteor.all.SigninSessionUpdate;
 
 @Component
 public class InterceptorFactory {
-	//실험
+	//실험2
 	@Autowired
 	SigninSessionUpdate signinSessionUpdate;
 	@Autowired
